f5951a985f34db4f528da7b2c4b7527ee66cd2b2
Luccas Lauth Gianesini <luccas.lauth@gmail.com>
2020-01-11 22:15:19 -0300

    Created

---
+++ src/Circa.Actors.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Circa.Actors.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Circa.Actors.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Circa.Actors.Infra
+{
+    public class Class1
+    {
+    }
+}
